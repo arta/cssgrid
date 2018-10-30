d4874e697b941d18cae4a7526fbd36b25445af48
Martin Fencl <martin.fencl@gmail.com>
2018-10-30 13:50:33 -0500

    add simple nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/index1', to: 'static_pages#index1'
-  get 'static_pages/index2', to: 'static_pages#index2'
+  get 'static_pages/index1', to: 'static_pages#index1', as: :index1
+  get 'static_pages/index2', to: 'static_pages#index2', as: :index2
 
   root 'static_pages#index1'
 end
